6ba894c8f869d39bf6b1bf6749c45f91950e4eee
Jaren Garman <jarengarman@protonmail.com>
2025-03-06 18:43:12 -0500

    feat: Validate uniqueness of coupon code

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,6 +1,6 @@
 class Coupon < ApplicationRecord
   validates :name, presence: true
-  validates :code, presence: true
+  validates :code, presence: true, uniqueness: true
   validates :type, presence: true
   validates :value, presence: true, numericality: true
   belongs_to :merchant
--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -4,6 +4,7 @@ describe Coupon, type: :model do
   describe "validations" do
     it { is_expected.to validate_presence_of :name }
     it { is_expected.to validate_presence_of :code }
+    it { is_expected.to validate_uniqueness_of :code }
     it { is_expected.to validate_presence_of :type }
     it { is_expected.to validate_presence_of :value }
     it { is_expected.to validate_numericality_of :value }
