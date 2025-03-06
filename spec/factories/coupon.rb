ff9fa365be2b71d603f7150d489c76ba41fac368
Jaren Garman <jarengarman@protonmail.com>
2025-03-06 18:56:00 -0500

    fix: Rename factory to coupon

--- spec/factories/coupon.rb
+++ spec/factories/coupon.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :customer do
+  factory :coupon do
     name { Faker::Commerce.promotion_code(digits: 2) }
     code { Faker::Commerce.unique.promotion_code }
     type { ["percent", "flat"].sample }
