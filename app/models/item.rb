eb786c10c5f363179e2eacb79174a2126ab59408
Jaren Garman <jarengarman@protonmail.com>
2025-03-06 18:23:58 -0500

    style: Standard style fix

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
   # has_many :invoices, through: :invoice_items
 
   def self.sort_by_price
-    self.order(unit_price: :asc)
+    order(unit_price: :asc)
   end
 
   def self.find_one_item_by_name(name)
