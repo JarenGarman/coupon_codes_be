5caf64511b6005dd5c0518b116b281573cf4bd2b
Jaren Garman <jarengarman@protonmail.com>
2025-03-07 16:17:55 -0500

    test: Add expect for coupon_id

--- spec/requests/api/v1/merchant/invoices_request_spec.rb
+++ spec/requests/api/v1/merchant/invoices_request_spec.rb
@@ -1,21 +1,23 @@
 require "rails_helper"
 
 RSpec.describe "Merchant invoices endpoints" do
-  before :each do
+  before do
     @merchant2 = Merchant.create!(name: "Merchant")
     @merchant1 = Merchant.create!(name: "Merchant Again")
+
+    @coupon = create(:coupon, merchant: @merchant1)
 
     @customer1 = Customer.create!(first_name: "Papa", last_name: "Gino")
     @customer2 = Customer.create!(first_name: "Jimmy", last_name: "John")
 
-    @invoice1 = Invoice.create!(customer: @customer1, merchant: @merchant1, status: "packaged")
+    @invoice1 = Invoice.create!(customer: @customer1, merchant: @merchant1, coupon: @coupon, status: "packaged")
     @invoice2 = Invoice.create!(customer: @customer1, merchant: @merchant1, status: "shipped")
     @invoice3 = Invoice.create!(customer: @customer1, merchant: @merchant1, status: "shipped")
     @invoice4 = Invoice.create!(customer: @customer1, merchant: @merchant1, status: "shipped")
     @invoice5 = Invoice.create!(customer: @customer1, merchant: @merchant2, status: "shipped")
   end
 
-  it "should return all invoices for a given merchant based on status param" do
+  it "returns all invoices for a given merchant based on status param" do
     get "/api/v1/merchants/#{@merchant1.id}/invoices?status=packaged"
 
     json = JSON.parse(response.body, symbolize_names: true)
@@ -26,10 +28,11 @@ RSpec.describe "Merchant invoices endpoints" do
     expect(json[:data][0][:type]).to eq("invoice")
     expect(json[:data][0][:attributes][:customer_id]).to eq(@customer1.id)
     expect(json[:data][0][:attributes][:merchant_id]).to eq(@merchant1.id)
+    expect(json[:data][0][:attributes][:coupon_id]).to eq(@coupon.id)
     expect(json[:data][0][:attributes][:status]).to eq("packaged")
   end
 
-  it "should get multiple invoices if they exist for a given merchant and status param" do
+  it "gets multiple invoices if they exist for a given merchant and status param" do
     get "/api/v1/merchants/#{@merchant1.id}/invoices?status=shipped"
 
     json = JSON.parse(response.body, symbolize_names: true)
@@ -38,7 +41,7 @@ RSpec.describe "Merchant invoices endpoints" do
     expect(json[:data].count).to eq(3)
   end
 
-  it "should only get invoices for merchant given" do
+  it "onlies get invoices for merchant given" do
     get "/api/v1/merchants/#{@merchant2.id}/invoices?status=shipped"
 
     json = JSON.parse(response.body, symbolize_names: true)
@@ -48,7 +51,7 @@ RSpec.describe "Merchant invoices endpoints" do
     expect(json[:data][0][:id]).to eq(@invoice5.id.to_s)
   end
 
-  it "should return 404 and error message when merchant is not found" do
+  it "returns 404 and error message when merchant is not found" do
     get "/api/v1/merchants/100000/customers"
 
     json = JSON.parse(response.body, symbolize_names: true)
@@ -59,7 +62,7 @@ RSpec.describe "Merchant invoices endpoints" do
     expect(json[:errors].first).to eq("Couldn't find Merchant with 'id'=100000")
   end
 
-  it "should return all invoices for a given merchant without status filter" do
+  it "returns all invoices for a given merchant without status filter" do
     get "/api/v1/merchants/#{@merchant1.id}/invoices"
 
     json = JSON.parse(response.body, symbolize_names: true)
