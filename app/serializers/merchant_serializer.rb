1756acbcb3f5223bb9d21cc8d7e29928998a4f7e
Jaren Garman <jarengarman@protonmail.com>
2025-03-07 17:02:40 -0500

    feat: Add coupons_count and invoice_coupon_count to serializer

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -2,9 +2,19 @@ class MerchantSerializer
   include JSONAPI::Serializer
   attributes :name
 
-  attribute :item_count, if: Proc.new { |merchant, params|
+  attribute :item_count, if: proc { |merchant, params|
     params && params[:count] == true
   } do |merchant|
     merchant.item_count
+  end
+
+  attributes :coupons_count do |merchant|
+    merchant.coupons.length
+  end
+
+  attributes :invoice_coupon_count do |merchant|
+    merchant.invoices.count do |invoice|
+      !invoice.coupon_id.nil?
+    end
   end
 end
