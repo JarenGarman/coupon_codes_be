a721f3dbd73de3f62e06378015adce36bc67ff07
Jaren Garman <jarengarman@protonmail.com>
2025-03-07 12:43:47 -0500

    feat: Add create endpoint with error handling

--- app/serializers/coupon_serializer.rb
+++ app/serializers/coupon_serializer.rb
@@ -1,4 +1,5 @@
 class CouponSerializer
   include JSONAPI::Serializer
-  attributes :name, :code, :discount_type, :value, :active?, :use_count
+  attributes :name, :code, :discount_type, :value, :active?
+  attributes :use_count, if: proc { |coupon| coupon[:use_count] }
 end
