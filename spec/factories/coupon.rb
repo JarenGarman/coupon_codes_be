4e383ba0a4363840cbcaf861a612472ce1b381b0
Jaren Garman <jarengarman@protonmail.com>
2025-03-07 11:50:54 -0500

    fix: Range does not include 0

--- spec/factories/coupon.rb
+++ spec/factories/coupon.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     name { Faker::Commerce.promotion_code(digits: 2) }
     code { Faker::Commerce.unique.promotion_code }
     discount_type { ["percent", "flat"].sample }
-    value { Faker::Commerce.price(range: 0..99.99) }
+    value { Faker::Commerce.price(range: 0.01..99.99) }
     active? { true }
     merchant
   end
