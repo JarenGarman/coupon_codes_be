96fa3e5c67fe21427a42b27abab6b7b29974c1aa
Jaren Garman <jarengarman@protonmail.com>
2025-03-07 12:43:47 -0500

    feat: Add create endpoint with error handling

--- app/controllers/api/v1/merchants/coupons_controller.rb
+++ app/controllers/api/v1/merchants/coupons_controller.rb
@@ -8,4 +8,30 @@ class Api::V1::Merchants::CouponsController < ApplicationController
     merchant = Merchant.find(params[:merchant_id])
     render json: CouponSerializer.new(merchant.coupons.with_use_count.find(params[:id]))
   end
+
+  def create
+    merchant = Merchant.find(params[:merchant_id])
+    if active_merchant_coupons(merchant) >= 5
+      render json: ErrorSerializer.too_many_active_coupons_response, status: :bad_request
+      return
+    end
+    coupon = merchant.coupons.create!(coupon_params) # safe to use create! here because our exception handler will gracefully handle exception
+    raise ActiveRecord::RecordInvalid.new unless coupon
+    render json: CouponSerializer.new(coupon), status: :created
+  end
+
+  private
+
+  def coupon_params
+    params
+      .require(:coupon)
+      .permit(:name, :code, :discount_type, :value)
+      .with_defaults(active?: true)
+  end
+
+  def active_merchant_coupons(merchant)
+    merchant.coupons.count do |coupon|
+      coupon.active?
+    end
+  end
 end
--- app/serializers/error_serializer.rb
+++ app/serializers/error_serializer.rb
@@ -1,15 +1,22 @@
 class ErrorSerializer
   def self.format_errors(messages)
     {
-      message: 'Your query could not be completed',
+      message: "Your query could not be completed",
       errors: messages
     }
   end
 
   def self.format_invalid_search_response
-    { 
-      message: "your query could not be completed", 
-      errors: ["invalid search params"] 
+    {
+      message: "your query could not be completed",
+      errors: ["invalid search params"]
+    }
+  end
+
+  def self.too_many_active_coupons_response
+    {
+      message: "Your query could not be completed",
+      errors: ["This merchant already has 5 active coupons"]
     }
   end
 end
