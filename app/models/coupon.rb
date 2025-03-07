ec158b92e286dab9bcc43bfd2d639de89d6d2665
Jaren Garman <jarengarman@protonmail.com>
2025-03-06 19:01:03 -0500

    feat: Add validation of discount_type value

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,7 +1,7 @@
 class Coupon < ApplicationRecord
   validates :name, presence: true
   validates :code, presence: true, uniqueness: true
-  validates :discount_type, presence: true
+  validates :discount_type, presence: true, inclusion: {in: ["percent", "flat"]}
   validates :value, presence: true, numericality: true
   belongs_to :merchant
   has_many :invoices
