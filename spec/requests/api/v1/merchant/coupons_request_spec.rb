6b285796b6e8f9774f2d398ac171f6aebdad777f
Jaren Garman <jarengarman@protonmail.com>
2025-03-07 14:47:30 -0500

    test: Add tests for update endpoint

--- spec/requests/api/v1/merchant/coupons_request_spec.rb
+++ spec/requests/api/v1/merchant/coupons_request_spec.rb
@@ -184,4 +184,110 @@ RSpec.describe "Merchant coupons endpoints" do
       expect(json[:errors].first).to eq("Validation failed: Value can't be blank, Value is not a number")
     end
   end
+
+  describe "update" do
+    it "can deactivate a coupon" do
+      merchant = create(:merchant)
+      coupon = create(:coupon, merchant: merchant)
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      patch "/api/v1/merchants/#{merchant.id}/coupons/#{coupon.id}", headers: headers, params: JSON.generate(coupon: {active?: false})
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(json[:data]).to be_a(Hash)
+      expect(json[:data][:id]).to be_a(String)
+      expect(json[:data][:type]).to eq("coupon")
+      expect(json[:data][:attributes]).to include(
+        name: coupon.name,
+        code: coupon.code,
+        discount_type: coupon.discount_type,
+        value: coupon.value,
+        active?: false
+      )
+    end
+
+    it "can activate a coupon" do
+      merchant = create(:merchant)
+      coupon = create(:coupon, merchant: merchant, active?: false)
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      patch "/api/v1/merchants/#{merchant.id}/coupons/#{coupon.id}", headers: headers, params: JSON.generate(coupon: {active?: true})
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+      expect(json[:data]).to be_a(Hash)
+      expect(json[:data][:id]).to be_a(String)
+      expect(json[:data][:type]).to eq("coupon")
+      expect(json[:data][:attributes]).to include(
+        name: coupon.name,
+        code: coupon.code,
+        discount_type: coupon.discount_type,
+        value: coupon.value,
+        active?: true
+      )
+    end
+
+    it "returns 404 and error message when merchant is not found" do
+      merchant = create(:merchant)
+      coupon = create(:coupon, merchant: merchant)
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      patch "/api/v1/merchants/100000/coupons/#{coupon.id}", headers: headers, params: JSON.generate(coupon: {active?: false})
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to have_http_status(:not_found)
+      expect(json[:message]).to eq("Your query could not be completed")
+      expect(json[:errors]).to be_a Array
+      expect(json[:errors].first).to eq("Couldn't find Merchant with 'id'=100000")
+    end
+
+    it "returns 404 and error message when coupon is not found" do
+      merchant = create(:merchant)
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      patch "/api/v1/merchants/#{merchant.id}/coupons/0", headers: headers, params: JSON.generate(coupon: {active?: false})
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to have_http_status(:not_found)
+      expect(json[:message]).to eq("Your query could not be completed")
+      expect(json[:errors]).to be_a Array
+      expect(json[:errors].first).to eq("Couldn't find Coupon with 'id'=0 [WHERE \"coupons\".\"merchant_id\" = $1]")
+    end
+
+    it "returns 422 and error message when params not provided" do
+      merchant = create(:merchant)
+      coupon = create(:coupon, merchant: merchant)
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      patch "/api/v1/merchants/#{merchant.id}/coupons/#{coupon.id}", headers: headers, params: JSON.generate(coupon: {})
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(json[:message]).to eq("Your query could not be completed")
+      expect(json[:errors]).to be_a Array
+      expect(json[:errors].first).to eq("Validation failed: Active? can't be blank")
+    end
+
+    it "returns 400 and error message when deactivating coupon with pending invoices" do
+      merchant = create(:merchant)
+      coupon = create(:coupon, merchant: merchant)
+      create(:invoice, status: "pending", merchant: merchant, coupon: coupon)
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      patch "/api/v1/merchants/#{merchant.id}/coupons/#{coupon.id}", headers: headers, params: JSON.generate(coupon: {active?: false})
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to have_http_status(:bad_request)
+      expect(json[:message]).to eq("Your query could not be completed")
+      expect(json[:errors]).to be_a Array
+      expect(json[:errors].first).to eq("Cannot deactivate coupon with pending invoices")
+    end
+  end
 end
