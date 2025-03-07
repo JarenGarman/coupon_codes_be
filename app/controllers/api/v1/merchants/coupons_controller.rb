f404e488da492ea9055f6ddc305a322c3ba89101
Jaren Garman <jarengarman@protonmail.com>
2025-03-07 11:52:44 -0500

    feat: Add index action

--- app/controllers/api/v1/merchants/coupons_controller.rb
+++ app/controllers/api/v1/merchants/coupons_controller.rb
@@ -1,4 +1,9 @@
 class Api::V1::Merchants::CouponsController < ApplicationController
+  def index
+    merchant = Merchant.find(params[:merchant_id])
+    render json: CouponSerializer.new(merchant.coupons.with_use_count)
+  end
+
   def show
     merchant = Merchant.find(params[:merchant_id])
     render json: CouponSerializer.new(merchant.coupons.with_use_count.find(params[:id]))
