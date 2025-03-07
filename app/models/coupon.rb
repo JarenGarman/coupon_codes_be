a4afc9bdf98b6b8f3ecc14e207f4f64970856597
Jaren Garman <jarengarman@protonmail.com>
2025-03-07 11:49:17 -0500

    fix: Validation of boolean with false value fails presence

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,9 +1,9 @@
 class Coupon < ApplicationRecord
   validates :name, presence: true
   validates :code, presence: true, uniqueness: true
-  validates :discount_type, presence: true, inclusion: {in: ["percent", "flat"]}
+  validates :discount_type, presence: true, inclusion: ["percent", "flat"]
   validates :value, presence: true, numericality: true
-  validates :active?, presence: true, inclusion: [true, false]
+  validates :active?, inclusion: [true, false]
   belongs_to :merchant
   has_many :invoices
 
